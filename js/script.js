998d78f5d83755ee2ac3d38c275ffcb6dbf32199
kelliaw <kelliwhiteweaver@gmail.com>
2022-03-29 21:11:57 -0400

    added the 8 ball code ---- looking to connect JSON with the script to parse locally...???

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@
       x.className = "topnav";
     }
   }
+
+  window.onload = function() {
+    var eight = document.getElementById("eight");
+    var answer = document.getElementById("answer");
+    var eightball = document.getElementById("eight-ball");
+    var question = document.getElementById("question");
+    
+    eightball.addEventListener("click", function() {
+    if (question.value.length < 1) {
+    alert('What is your question?');
+    } else {
+    eight.innerText = "";
+    var num = Math.floor(Math.random() * Math.floor(answers.length));
+    answer.innerText = answers[num];
+    }
+    });
+    };
